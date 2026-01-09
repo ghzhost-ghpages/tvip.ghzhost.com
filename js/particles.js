fefbc6deebbad3b6959083cc018f5ebc0e5245c6
Your Name <you@example.com>
2026-01-08 23:36:48 -0300

    Initial commit

---
+++ js/particles.js
@@ -0,0 +1,25 @@
+
+
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 250    // Quantidade de partículas
+        },
+        "line_linked": {
+            "color": "#0f2b46", // Cor das ligações
+            "opacity": 100
+        },
+
+        "size": {
+            "value":0, // Tamanho das partículas (0 para remover as bolas)
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "repulse"
+            }
+        }
+    }
+});
---
+++ js/vanta.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x34789,
+    shininess: 50,
+    waveHeight: 20.50,
+    waveSpeed: 1.05,
+    zoom: 1.5
+  });
